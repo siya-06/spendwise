af28ef1c87f8d1bf6639ebb562e9f4d19cbd0558
siya <siyasri0604@gmail.com>
2025-08-26 01:16:17 +0530

    added dashboard/expense routes and controllers, updated backend

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,4 +1,4 @@
-const User=require('../models/User')
+const xlsx =require('xlsx')
 const Income=require('../models/Income')
 
 
@@ -30,19 +30,50 @@ exports.addIncome=async(req,res) =>{
 
 
 //get all income source
-exports.getAllIncome=async(req,res) =>{}
-
-
+exports.getAllIncome=async(req,res) =>{
+    const userId=req.user.id
+    
+    try{
+        const income=await Income.find({userId}).sort({date:-1})
+        res.json(income)
+    }catch (error){
+        res.status(500).json({message: "syntax error"})
+    }
+}
 
 
 //delete income source
-exports.deleteIncome=async(req,res) =>{}
-
-
-
+exports.deleteIncome=async(req,res) =>{
+    try{
+        await Income.findByIdAndDelete(req.params.id)
+        res.json({message:"Income deleted successfully"})
+    }catch(error){
+        res.status(500).json({message:"Server Error"})
+    }
+}
 
 //download excel
-exports.downloadIncomeExcel=async(req,res) =>{}
+exports.downloadIncomeExcel=async(req,res) =>{
+    const userId =req.user.id
+    try{
+        const income=await Income.find({userId}).sort({date:-1})
+
+        //prepare data for excel
+        const data=income.map((item) =>({
+            Source:item.source,
+            Amount:item.amount,
+            Date: item.date,
+        }))
+
+        const wb= xlsx.utils.book_new()
+        const ws=xlsx.utils.json_to_sheet(data)
+        xlsx.utils.book_append_sheet(wb,ws,"Income")
+        xlsx.writeFile(wb,'income_details.xlsx')
+        res.download('income_details.xlsx')
+    }catch(error){
+        res.status(500).json({message:"server error"})
+    }
+}
 
 
 
